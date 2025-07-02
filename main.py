29298e2a15bef4cd9d5cba737a6c17c81eb24bee
James St-Pierre <jamesamstpierre@gmail.com>
2025-07-02 07:57:30 -0400

    Add grouping to pygame elements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-from constants import *
-from player import *
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
@@ -8,6 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -15,10 +21,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for elem in drawable:
+            elem.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
